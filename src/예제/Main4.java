8335edf635977ecb4c082d3c8d054e0c47a94d95
Subeen Jeon <145312273+subeenjeonHere@users.noreply.github.com>
2024-04-26 10:49:03 +0900

    Add

--- src/예제/Main4.java
+++ src/예제/Main4.java
@@ -3,17 +3,29 @@ package 예제;
 class N {
     private int a;
 
+    public N() {
+        System.out.println("N");
+    }
+
     public N(int a) {
         this.a = a;
+
+
     }
 
     public void display() {
         System.out.println("a=" + a);
         this.a = 1000;
     }
+
 }
 
 class M extends N {
+    public M() {
+        System.out.println("M");
+
+    }
+
     public M(int a) {
         super(a);
         super.display();
@@ -24,6 +36,7 @@ class M extends N {
 public class Main4 {
     public static void main(String[] args) {
         M obj = new M(10); // 생성자 호출
+        N obj2 = new M();
 
 
     }
